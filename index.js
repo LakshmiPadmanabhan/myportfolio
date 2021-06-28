30ff5bae7ad21e8c55a6c907bafc88ddc52aeaa4
P LAKSHMI <lakshmipadmanabhan2000@gmail.com>
2021-06-28 17:54:14 +0530

    My Portfolio

---
+++ index.js
@@ -0,0 +1,8 @@
+
+var v1= document.getElementById('mybtn')
+v1.onclick=function(){
+    location.href="https://www.linkedin.com/in/lakshmi-padmanabhan-7ab8881a1/"
+}
+function fn(){
+    swal("Welcome","Please have a look","success")
+}
